6832ae18e897b810fd52dfce41a5af5e981ce46b
Grace Gude <grace@Graces-MBP.localdomain>
2019-02-16 12:24:03 -0700

    updating images and json

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <>
             <footer className="fixed-bottom">
                 <div className="container">
-                    &copy; 2019 | Made with <i className="em em-hearts"></i> and <i className="fab fa-react react-icon"></i> by <a href="https://github.com/mggude">mggude</a>
+                    &copy; 2019 | Made with <i className="em em-hearts"></i> and <i className="fab fa-react react-icon"></i> by <a href="https://github.com/mggude">mggude</a> <i className="em em-pray"></i> Rider Waite Tarot | <a href="https://www.tarotcardmeanings.net/tarotcards.htm" target="_blank">tarotcardmeanings.net</a>
                 </div>
             </footer>
         </>
